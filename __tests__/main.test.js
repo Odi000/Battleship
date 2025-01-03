07a0649c3807b639139825df251b38039912139d
Odi000 <mandimandi.ro@gmail.com>
2025-01-03 11:45:42 +0100

    receiveAttack() method tests are written

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -27,9 +27,20 @@ test("Board should have 100 coordinates", () => {
 test("Place ship in specific coordinates", () => {
     const gameBoard = new game.GameBoard();
     const liburna = new game.Ship(3)
+    
     expect(gameBoard.placeShip(liburna, -1, 0, "vertical")).toBe(false);
     expect(gameBoard.placeShip(liburna, 0, 0, "vertical")).toEqual([[0, 0], [0, 1], [0, 2]]);
     expect(gameBoard.placeShip(liburna, 0, 0, "horizontal")).toEqual([[0, 0], [1, 0], [2, 0]]);
     expect(gameBoard.placeShip(liburna, 7, 8, "horizontal")).toEqual([[7, 8], [8, 8], [9, 8]]);
     expect(gameBoard.placeShip(liburna, 7, 8, "vertical")).toBe(false);
+});
+
+test("determines whether or not the attack hit a ship or not", () => {
+    const gameBoard = new game.GameBoard();
+    const liburna = new game.Ship(3)
+
+    expect(gameBoard.receiveAttack(0,0)).toBe(true);
+    expect(gameBoard.receiveAttack(5,5)).toBe(false);
+    expect(liburna.hitsTaken).toBe(1);
+    expect(gameBoard.board[55]).toBe("missed-hit");
 })
